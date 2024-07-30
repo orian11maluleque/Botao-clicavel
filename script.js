6795741b5a46d90dc01f0e6e868bf7eeeff75917
Orian Maluleque <orianyvesmaluleque@gmail.com>
2024-07-30 06:35:36 +0200

    ponto de paregem
    se der merda volta para aqui e funcionavel e da para qualquer cena

---
+++ script.js
@@ -0,0 +1,8 @@
+const buttonHeight = 150;
+const buttonwidth = 50;
+
+window.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button');
+
+    button.addEventListener('click', () => alert('Clicaste! mas o nome não muda sera Edinho para sempre'))
+});
